00cd395f1b7d956703f64944ddb6d16f0186565c
Jakob Hohenstein <jakob-hohenstein@gmx.de>
2022-04-23 22:03:49 +0200

    challenge 3

--- 04-Decision-Science/olist/data.py
+++ 04-Decision-Science/olist/data.py
@@ -14,7 +14,30 @@ class Olist:
             # Use __file__ instead as an absolute path anchor independant of your usename
             # Make extensive use of `breakpoint()` to investigate what `__file__` variable is really
         # Hint 2: Use os.path library to construct path independent of Mac vs. Unix vs. Windows specificities
-        pass  # YOUR CODE HERE
+
+        # go up two levels from current directory and save directory name
+        root_dir = os.path.dirname(os.path.dirname(__file__))
+        # concatenate root_dir with relative path to csv files
+        csv_path = os.path.join(root_dir, "data", "csv")
+
+
+        # read in names of files in data folder
+        file_names = []
+        for file in os.listdir(csv_path):
+            if file.endswith(".csv"):
+                file_names.append(file)
+
+        # for the key names unnecessary string elements are removed
+        key_names = []
+        for file in file_names:
+            key_names.append(file.replace(".csv", "").replace("olist_", "").replace("_dataset",""))
+
+        # create dict
+        data = dict()
+        for key, file in zip(key_names, file_names):
+            data[key] = pd.read_csv(os.path.join(csv_path, file))
+
+        return data
 
     def ping(self):
         """
